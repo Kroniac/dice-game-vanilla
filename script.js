d4a344cb7d83dfb1a4df663ff81d19235ed858eb
Farid Ansari <farid12ansari7@gmail.com>
2017-12-14 21:29:13 +0530

    random dice number with img on roll dice button click

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.querySelector('.dice').style.display = 'none';
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  console.log('Roll button is clicked');
+  dice = Math.floor(Math.random() * 6) + 1;
+  document.querySelector('.dice').style.display = 'block';
+  document.querySelector('.dice').src = 'dice-' + dice + '.png';
 });
