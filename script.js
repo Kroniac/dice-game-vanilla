bc7ab410667e3b831e639926ce79b3c09fcc3e08
Farid Ansari <farid12ansari7@gmail.com>
2017-12-15 15:25:34 +0530

    changing active class in player change and setting total score on hold and change player

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+let activePlayer = 0;
+let roundScore = 0;
 
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
 document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
-
-
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -12,4 +12,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   dice = Math.floor(Math.random() * 6) + 1;
   document.querySelector('.dice').style.display = 'block';
   document.querySelector('.dice').src = 'dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('active');
+    
+  }
+});
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  document.getElementById('score-' + activePlayer).textContent =
+    Number(document.getElementById('score-' + activePlayer).textContent) +
+    roundScore;
+  roundScore = 0;
+  document.getElementById('current-' + activePlayer).textContent = roundScore;
+  document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');  
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector('.player-'+activePlayer+'-panel').classList.add('active');
+  
 });
